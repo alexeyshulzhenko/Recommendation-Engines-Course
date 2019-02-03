bc918c66f7b31a9fe5cac850b943f38e84161226
alexeyshulzhenko <lexa270@gmail.com>
2019-02-03 18:03:45 +0200

    Built algorythm itself

--- Lab01/Lab1.py
+++ Lab01/Lab1.py
@@ -5,38 +5,23 @@ import csv
 from math import log
 
 # Addind service DATETIME Vars
-
 now = datetime.datetime.now()
 epoch = datetime(1970, 1, 1)
+
+
+
 tittle = []
 date = []
-upVotes = []
-downVotes = []
+score_index = []
 rank = []
 
-def main():
-    with open('HW.csv', 'rt', encoding='utf8') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-        for row in spamreader:
-            tittle.append(row[0])
-            date.append(epoch_seconds(row[1]))
-            upVotes.append(row[2])
-            downVotes.append(row[3])
 
-
-
-
-if __name__ == '__main__':
-    main()
-
-
-
-
-#We
+#We translate timestamp to epoch seconds
 def epoch_seconds(date):
     td = date - epoch
     return td.days * 86400 + td.seconds + (float(td.microseconds) / 1000000)
 
+#We calculate score, based on upvotes and downvotes
 def score(ups, downs):
     return ups - downs
 
@@ -47,5 +32,23 @@ def hot(ups, downs, date):
     seconds = epoch_seconds(date) - 1134028003
     return round(sign * order + seconds / 45000, 7)
 
-print(hot(10, 100, datetime(2000, 1, 1)))
-print(hot(10, 1000, datetime(2000, 1, 1)))
+
+def main():
+    with open('HW.csv', 'rt', encoding='utf8') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        for row in spamreader:
+            tittle.append(row[0])
+            date.append(epoch_seconds(row[1]))
+            score_index.append(score(row[2], row[3]))
+            rank.append(hot(row[2], row[3],row[1]))
+
+    sorted = [x for _,x in sorted(zip(rank,tittle))]
+    for row in sorted:
+        print(row)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
